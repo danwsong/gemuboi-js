1f4e951c2925320905524bd8cc1c8e7a502f454d
Dan Song <dan.w.song@gmail.com>
2021-01-05 11:56:14 -0500

    Fix joypad bug when reading both inputs

--- public/js/joypad.js
+++ public/js/joypad.js
@@ -17,18 +17,18 @@ class Joypad {
     }
 
     get p1() {
-        let _p1 = 0xc0 | (this.button << 5) | (this.direction << 4);
-        if (!this.button) {
-            _p1 |= (!this.start << 3) | (!this.select << 2) | (!this.b << 1) | !this.a;
+        let _p1 = (!this.button << 5) | (!this.direction << 4);
+        if (this.button) {
+            _p1 |= (this.start << 3) | (this.select << 2) | (this.b << 1) | this.a;
         }
-        if (!this.direction) {
-            _p1 |= (!this.down << 3) | (!this.up << 2) | (!this.left << 1) | !this.right;
+        if (this.direction) {
+            _p1 |= (this.down << 3) | (this.up << 2) | (this.left << 1) | this.right;
         }
-        return _p1;
+        return 0xc0 | (_p1 ^ 0x3f);
     }
 
     set p1(value) {
-        this.button = (value & 0x20) != 0;
-        this.direction = (value & 0x10) != 0;
+        this.button = (value & 0x20) == 0;
+        this.direction = (value & 0x10) == 0;
     }
 }
