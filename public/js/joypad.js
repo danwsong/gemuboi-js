a3ccb5524a479dfdcbe91f2eb602b797a382aa61
Dan Song <dan.w.song@gmail.com>
2021-01-05 11:31:23 -0500

    Refactor joypad implementation

--- public/js/joypad.js
+++ public/js/joypad.js
@@ -2,8 +2,8 @@ class Joypad {
     constructor(gb) {
         this.gb = gb;
 
-        this.selectButton = false;
-        this.selectDirection = false;
+        this.button = false;
+        this.direction = false;
 
         this.start = false;
         this.select = false;
@@ -17,26 +17,18 @@ class Joypad {
     }
 
     get p1() {
-        let _p1 = 0b111111;
-        _p1 ^= this.selectButton << 5;
-        _p1 ^= this.selectDirection << 4;
-        if (this.selectButton) {
-            _p1 ^= this.start << 3;
-            _p1 ^= this.select << 2;
-            _p1 ^= this.b << 1;
-            _p1 ^= this.a;
+        let _p1 = 0xc0 | (this.button << 5) | (this.direction << 4);
+        if (!this.button) {
+            _p1 |= (!this.start << 3) | (!this.select << 2) | (!this.b << 1) | !this.a;
         }
-        if (this.selectDirection) {
-            _p1 ^= this.down << 3;
-            _p1 ^= this.up << 2;
-            _p1 ^= this.left << 1;
-            _p1 ^= this.right;
+        if (!this.direction) {
+            _p1 |= (!this.down << 3) | (!this.up << 2) | (!this.left << 1) | !this.right;
         }
         return _p1;
     }
 
     set p1(value) {
-        this.selectButton = (value & 0b100000) == 0;
-        this.selectDirection = (value & 0b10000) == 0;
+        this.button = (value & 0x20) != 0;
+        this.direction = (value & 0x10) != 0;
     }
 }
